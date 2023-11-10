db9938a34debf3b803451791c91aca991abb1353
Xin <xzhao34@slb.com>
2023-11-10 11:30:19 +0800

    feat: add todoItem component

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { ToDoItem } from 'src/model/ToDoItem';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent {
+  items: ToDoItem[] = [
+    {      
+      id: 1,
+      title: 'sleep',
+      description: 'go to bed early',
+      isDone: false
+    },
+    {      
+      id: 2,
+      title: 'buy bread',
+      description: 'yyyy',
+      isDone: false
+    }
+  ]
+}
