81ac9460d345918da0dbada7289b7c05f83fea72
Yichen Zhang <yzhang183@slb.com>
2023-11-10 13:09:58 +0800

    feat: add create-todo component

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -4,9 +4,11 @@ import { ToDoItem } from 'src/model/ToDoItem';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
-  items: ToDoItem[] = [{id: 0, title: 'buy milk', description:'ababa', isDone:false},
-   {id: 1, title: 'buy cookie', description:'ababa', isDone:false}]
+  items: ToDoItem[] = [
+    { id: 0, title: 'buy milk', description: 'ababa', isDone: false },
+    { id: 1, title: 'buy cookie', description: 'ababa', isDone: false },
+  ];
 }
