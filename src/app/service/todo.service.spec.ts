c2b3d778cb62d7d17c36db174b9851a452e6bfcf
Jinglin Wang <JWang317@slb.com>
2023-11-10 14:21:16 +0800

    test: add test for getAll

--- src/app/service/todo.service.spec.ts
+++ src/app/service/todo.service.spec.ts
@@ -8,9 +8,27 @@ describe('TodoService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
+    service.items = [{
+      id: 1,
+      description: "test-desc",
+      title: "test-title",
+      isDone: false
+    }]
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it("should get all items when call getAll",()=>{
+    const items = service.getAll();
+    expect(items).toEqual(
+      [{
+        id: 1,
+        description: "test-desc",
+        title: "test-title",
+        isDone: false
+      }]
+    )
+  })
 });
