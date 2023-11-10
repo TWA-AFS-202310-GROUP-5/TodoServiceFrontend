674acb3d8c71410a4f232777a5152f854d4d6020
Xin <xzhao34@slb.com>
2023-11-10 16:30:28 +0800

    test: add todoHttp service test

--- src/app/service/todo-http.service.spec.ts
+++ src/app/service/todo-http.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TodoHttpService } from './todo-http.service';
+import { HttpClient } from '@angular/common/http';
+import { defer } from 'rxjs';
+
+function asyncData<T>(data: T) {
+  return defer(() => Promise.resolve(data));
+}
 
 describe('TodoHttpService', () => {
   let service: TodoHttpService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>; // declare object to test
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(TodoHttpService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']); //pass value to object
+    //service = TestBed.inject(TodoHttpService);
+    service = new TodoHttpService(httpClientSpy);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get all items when call getAll', () => {
+    httpClientSpy.get.and.returnValue(
+      asyncData([
+        {
+          id: 0,
+          title: 'Home work',
+          description: 'Have to complete home work',
+          isDone: false,
+        },
+      ])
+    );
+
+    service.getAll().subscribe((data) => {
+      expect(data.length).toEqual(1);
+    });
+
+    expect(httpClientSpy.get.calls.count()).toEqual(1);
   });
 });
