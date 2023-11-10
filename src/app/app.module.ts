1e28d91a118821f6fc11f6c18742e30f1899b59c
Yichen Zhang <yzhang183@slb.com>
2023-11-10 14:54:08 +0800

    feat: add routing for list and detail page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { CreateTodoComponent } from './create-todo/create-todo.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TodoDetailComponent } from './todo-detail/todo-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoListComponent, CreateTodoComponent],
+  declarations: [AppComponent, TodoListComponent, CreateTodoComponent, TodoDetailComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
