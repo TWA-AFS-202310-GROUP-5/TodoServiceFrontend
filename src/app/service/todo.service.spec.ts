271fbda824829c3f1e1fa453526edfd15ad69a46
Zhiping Shu <ZShu@slb.com>
2023-11-10 14:21:13 +0800

    test: add a service getall test

--- src/app/service/todo.service.spec.ts
+++ src/app/service/todo.service.spec.ts
@@ -8,9 +8,29 @@ describe('TodoService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
+    service.items = [
+      {
+        id: 1,
+        title: 'buy milk',
+        description: 'buy some milk',
+        isDone: false,
+      }
+    ]
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get all items when call getAll', () => {
+    const item = service.getAll()
+    expect(item).toEqual([
+      {
+        id: 1,
+        title: 'buy milk',
+        description: 'buy some milk',
+        isDone: false,
+      }
+    ])
+  })
 });
