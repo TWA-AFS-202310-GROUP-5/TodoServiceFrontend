5582785475981ae053f8776b43c76fcf046766ef
Yang Liu <YLiu148@slb.com>
2023-11-10 13:10:29 +0800

    feat: add create todo item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { CreateTodoComponent } from './create-todo/create-todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    CreateTodoComponent
   ],
   imports: [
     BrowserModule,
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -4,22 +4,21 @@ import { ToDoItem } from 'src/model/ToDoItem';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
-  items:ToDoItem[] = [
+  items: ToDoItem[] = [
     {
-      id:0,
-      title:"buy milk",
-      description:"pure mulk",
-      isDone:false
+      id: 0,
+      title: 'buy milk',
+      description: 'pure mulk',
+      isDone: true,
     },
     {
-      id:1,
-      title:"buy bread",
-      description:"black bread",
-      isDone:false
-    }
+      id: 1,
+      title: 'buy bread',
+      description: 'black bread',
+      isDone: false,
+    },
   ];
-
 }
