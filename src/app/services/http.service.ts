4a4f9173ed3bdd90f6b990b28856d64d3ea48678
Yang Liu <YLiu148@slb.com>
2023-11-10 16:06:03 +0800

    feat: connect to backend

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ToDoItem } from 'src/model/ToDoItem';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  url:string = 'https://localhost:44309/ToDoItem';
+  constructor(private httpClient:HttpClient) { }
+
+  getAll() {
+    return this.httpClient.get<ToDoItem[]>(this.url)
+  }
+}
