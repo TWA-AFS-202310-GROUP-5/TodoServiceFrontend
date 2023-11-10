98f5eaa7825d5358e0c7f8eaf55efb1abb34c508
Yichen Zhang <yzhang183@slb.com>
2023-11-10 16:05:02 +0800

    feat: add todo-http service to connect with backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,21 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { CreateTodoComponent } from './create-todo/create-todo.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TodoDetailComponent } from './todo-detail/todo-detail.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, TodoListComponent, CreateTodoComponent, TodoDetailComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    TodoListComponent,
+    CreateTodoComponent,
+    TodoDetailComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
