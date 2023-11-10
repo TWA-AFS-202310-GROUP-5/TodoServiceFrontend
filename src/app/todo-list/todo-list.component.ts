4f1af5ca017ca6d2e4fd9929ab2d37702e44ead8
Zhiping Shu <ZShu@slb.com>
2023-11-10 15:17:37 +0800

    feat: add a view detail button on todoList

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ToDoItem } from 'src/model/ToDoItem';
 import { TodoService } from '../service/todo.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo-list',
@@ -10,7 +11,7 @@ import { TodoService } from '../service/todo.service';
 export class TodoListComponent {
   items: ToDoItem[] = [];
 
-  constructor(private todoService: TodoService) {}
+  constructor(private todoService: TodoService, private router: Router) {}
 
   ngOnInit() {
     this.items = this.todoService.getAll();
@@ -18,5 +19,9 @@ export class TodoListComponent {
 
   onMarkDone(id: number){
     this.todoService.markDone(id)
+  }
+
+  onGoToDetail(id: number){
+    this.router.navigateByUrl(`/detail/${id}`)
   }
 }
