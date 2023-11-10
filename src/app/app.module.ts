04e01b51b6dfbacaebf3142d16e9c64964d29350
Zhiping Shu <ZShu@slb.com>
2023-11-10 14:54:19 +0800

    feat: add router and detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { CreateTodoComponent } from './create-todo/create-todo.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TodoDetailComponent } from './todo-detail/todo-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoListComponent, CreateTodoComponent],
-  imports: [BrowserModule, 
-    AppRoutingModule, 
-    ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    TodoListComponent,
+    CreateTodoComponent,
+    TodoDetailComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
