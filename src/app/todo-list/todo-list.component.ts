cacbc407272d39e59a47183909b06b2c924c9967
Yang Liu <YLiu148@slb.com>
2023-11-10 11:30:02 +0800

    feat: add todolist component

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { ToDoItem } from 'src/model/ToDoItem';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent {
+  items:ToDoItem[] = [
+    {
+      id:0,
+      title:"buy milk",
+      description:"pure mulk",
+      isDone:false
+    },
+    {
+      id:1,
+      title:"buy bread",
+      description:"black bread",
+      isDone:false
+    }
+  ];
+
+}
